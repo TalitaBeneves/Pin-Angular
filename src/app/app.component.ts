25ac116ccafef73289b1da77f8c18b88feb946d9
Talita Beneves <talitabeneves15@gmail.com>
2022-12-27 20:07:42 -0300

    desabilitando datas anteriores datepicker + pt-BR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+  minDate: any;
+  maxDate: any;
+  date: any;
+  valorTela: any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.montaForm();
+    this.minDate = new Date();
+  }
+
   // this called every time when user changed the code
   onCodeChanged(code: string) {
-    console.log("Change",code)
+    console.log('Change', code);
   }
 
   // this called only if user entered full code
   onCodeCompleted(code: string) {
-    console.log("Completed",code)
+    console.log('Completed', code);
+  }
 
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
+
+  montaForm() {
+    this.form = this.fb.group({
+      data: [new Date(), Validators.required],
+    });
+  }
+
+  confirma() {
+    this.valorTela = this.form.get('data')?.value;
+
+    console.log(this.valorTela);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CodeInputModule } from 'angular-code-input';
-
+import { LOCALE_ID } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    CodeInputModule
+    BrowserAnimationsModule,
+    CodeInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt-Br' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
